1fedc185c1e668681df2896000ccf24f2b5c708f
Francis <francis.ozua@gmail.com>
2023-03-21 19:10:17 -0500

    YOU CAN NOW SEE PICTURES ON FRONTEND - i put all images into the public folder then boom, we win

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,16 +1,25 @@
+import { useState, useEffect } from "react"
 import "./Post.css"
-
 const Post = ({ post, removePost }) => {
+    
+    const [image, setImage] = useState(false)
+    document.body.style.backGround = "red"
+
+    useEffect(() => {
+        if (post.image) setImage(true)
+        console.log(post.image.substring(46))
+
+    }, [])
 
     return (
         <div id="card">
             <h1>Topic : {post.topic}</h1>
             <h2>Post by {post.username}</h2>
+            {image && <img className="image" src={`../${post.image.substring(46)}`} alt="lol"></img>}
             <p>{post.info}</p>
             <button onClick={() => removePost(post.id)}>DELETE</button>
         </div>
         
     )
 }
-
 export default Post
